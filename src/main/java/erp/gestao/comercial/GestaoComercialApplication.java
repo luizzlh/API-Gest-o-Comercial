9c3044fcd60938bc8f29fc43e025df5cb8ac2106
Luiz <luizzlh2004@gmail.com>
2026-02-23 22:23:31 -0300

    Início do projeto.

---
+++ src/main/java/erp/gestao/comercial/GestaoComercialApplication.java
@@ -0,0 +1,13 @@
+package erp.gestao.comercial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoComercialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoComercialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/erp/gestao/comercial/GestaoComercialApplicationTests.java
@@ -0,0 +1,13 @@
+package erp.gestao.comercial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoComercialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
