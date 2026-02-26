8287b7ef76dd5a1e3aaf0de54350af63fc2e3778
Luiz Henrique <106248378+luizzlh@users.noreply.github.com>
2026-02-26 14:54:47 -0300

    Atualizando entidade ItemPedido quanto a relacionamento

--- src/main/java/erp/comercial/demo/entities/ItemPedido.java
+++ src/main/java/erp/comercial/demo/entities/ItemPedido.java
@@ -3,25 +3,18 @@ package erp.comercial.demo.entities;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name="itens_pedido")
 public class ItemPedido {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private int id;
-    private int numeroPedido;
     @ManyToOne
-    @JoinColumn(name = "produto_codigo_produto")
+    @JoinColumn(name = "order_id", nullable = false)
+    private Pedido pedido;
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
     private Produto produto;
     private int quantidade;
     private double precoUnitario;
     private double precoTotal;
-
-    public Produto getProduto() {
-        return produto;
-    }
-
-    public void setProduto(Produto produto) {
-        this.produto = produto;
-    }
 
 }
