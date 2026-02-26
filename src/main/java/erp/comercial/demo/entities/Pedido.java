f394e1e7b1bff3d7cfb2f99dcfc62bfad27f4e5b
Luiz Henrique <106248378+luizzlh@users.noreply.github.com>
2026-02-26 14:56:27 -0300

    Atualizando entidade Pedido quanto aos relacionamentos e atributos.

--- src/main/java/erp/comercial/demo/entities/Pedido.java
+++ src/main/java/erp/comercial/demo/entities/Pedido.java
@@ -3,17 +3,26 @@ package erp.comercial.demo.entities;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
-@Table(name="pedidos")
 public class Pedido {
     @Id
     @Column(name="numeroPedido")
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private int numeroPedido;
+    @ManyToOne
+    @JoinColumn(name = "cliente_id", nullable = false)
     private Cliente cliente;
+    @ManyToOne
+    @JoinColumn(name = "vendedor_id", nullable = false)
     private Usuario vendedor;
-    private Status status;
+    //private Status status;
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ItemPedido> items = new ArrayList<>();
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Pagamento pagamento;
     private double totalPedido;
     private LocalDateTime horaCriacao;
     private LocalDateTime horaAtualizacao;
