a1d66d05f4ad6dfd51c0cafe1c65d7dd34fcc68d
Luiz Henrique <106248378+luizzlh@users.noreply.github.com>
2026-02-26 14:54:18 -0300

    Atualizando entidade cliente.
    Removed unnecessary table annotation and added a newline at the end of the file.

--- src/main/java/erp/comercial/demo/entities/Cliente.java
+++ src/main/java/erp/comercial/demo/entities/Cliente.java
@@ -1,11 +1,9 @@
 package erp.comercial.demo.entities;
 
 import jakarta.persistence.*;
-
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name="cliente")
 public class Cliente {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
