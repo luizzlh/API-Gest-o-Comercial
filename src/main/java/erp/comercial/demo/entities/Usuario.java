6ef4698f6dc9bc929b348d0047db14d2ffc7d1b3
Luiz Henrique <106248378+luizzlh@users.noreply.github.com>
2026-02-26 14:57:40 -0300

    Atualizando entidade Usuario quanto a relacionamento e atributos.

--- src/main/java/erp/comercial/demo/entities/Usuario.java
+++ src/main/java/erp/comercial/demo/entities/Usuario.java
@@ -3,18 +3,34 @@ package erp.comercial.demo.entities;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
-@Table(name="usuarios")
+@Table(name = "users")
 public class Usuario {
+
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String nome;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
     private String email;
-    private String senha;
-    private Boolean ativo;
-    //private Role role;
-    private LocalDateTime dataCriacao;
-    private LocalDateTime dataAtualizacao;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private Boolean active = true;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
 }
