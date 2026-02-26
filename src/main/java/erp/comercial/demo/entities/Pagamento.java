dc3fe5a2314177d797f578a937f92e4aae80414c
Luiz Henrique <106248378+luizzlh@users.noreply.github.com>
2026-02-26 14:55:37 -0300

    Atualizando entidade de Pagamento
    Removed unused fields and added relationship to Pedido.

--- src/main/java/erp/comercial/demo/entities/Pagamento.java
+++ src/main/java/erp/comercial/demo/entities/Pagamento.java
@@ -5,14 +5,15 @@ import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name="Pagamento")
 public class Pagamento {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private int id;
+    @OneToOne
+    @JoinColumn(name = "order_id", nullable = false, unique = true)
     private Pedido pedido;
     private double valor;
-    private StatusPagamento statusPagamento;
-    private FormaPagamento formaPagamento;
+    //private StatusPagamento statusPagamento;
+    //private FormaPagamento formaPagamento;
     private LocalDateTime horarioPagamento;
 }
